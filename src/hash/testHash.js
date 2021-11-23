196ef0c50013f05ea743983def8c1ed6e615f3e8
angelicagrass <angelicagrass@hotmail.com>
2021-11-23 16:59:09 +0100

    test small variations in string

--- src/hash/testHash.js
+++ src/hash/testHash.js
@@ -1,13 +1,11 @@
 import Hash from './hash.js';
 
-export default function testHash() {
+export function testHash() {
     let errors = 0
 
     for (let i = 0; i < 1000; i++) {
         const randomString = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-
         const hash = new Hash(randomString);
-        //testObject[randomString] = hash.hashValue;
         if(hash.hashValue < 0 || hash.hashValue > 256) {
             errors++
         }
@@ -17,6 +15,33 @@ export default function testHash() {
         console.log('test passed, all the hashvalues is between 0-256')
     } else {
         console.log('test failed, there are ' + errors + ' hashvalues that is not between 0-256')
+    }
+}
+
+export function testSmallVariations() {
+    const strings = []
+    const hashValues = []
+    
+
+    let str = 'hello, this is my teststring that tests small variations!'
+    
+
+    for (let i = 0; i < 10; i++) {
+        let randomNumber = Math.floor(Math.random() * (str.length - 1))
+
+        let testString = str.substring(Math.random().toString(36).substring(2, 15)) + str.substring(randomNumber, str.length);
+
+        
+        testString = str.split('').map((char, index) => {
+            if (index === randomNumber) {
+                return char.toUpperCase()
+            } else {
+                return char
+            }
+        }).join('')
+        console.log(testString)
+        //const hash = new Hash(testString);
+        //console.log(hash.hashValue)
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import Hash from './hash/hash.js';
-import testHash from './hash/testHash.js';
+import {testHash, testSmallVariations} from './hash/testHash.js';
 
 //let myHash = new Hash('hyvelost');
 //console.log(myHash)
 
 let test = testHash();
+let testSmall = testSmallVariations();
 
 
 
