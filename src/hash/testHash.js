d815fab7d8a0fdef3db97000e42a37044c030155
angelicagrass <angelicagrass@hotmail.com>
2021-11-23 10:57:41 +0100

    Test for hash

--- src/hash/testHash.js
+++ src/hash/testHash.js
@@ -1,11 +1,12 @@
-import Hash from '../hash.js';
+import Hash from './hash.js';
 
 // test hash  -   hash.js
-function testHash() {
-    const hash = new Hash();
+export default function testHash() {
+    const hash = new Hash('teststring');
     // create random string
     const randomString = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-
     console.log(randomString)
 
+    return randomString
 }
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import Hash from './hash/hash.js';
+import testHash from './hash/testHash.js';
 
 let myHash = new Hash('hyvelost');
 console.log(myHash)
+
+let test = testHash();
+
+console.log(test)
+
